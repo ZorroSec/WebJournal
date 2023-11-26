76605a7ef5113a8e9bdc69ab04fa0b8204103603
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-11-26 10:50:09 -0300

    add.php

--- pages/add.php
+++ pages/add.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
