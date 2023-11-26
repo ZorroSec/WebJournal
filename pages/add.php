a60649beae7fca6284b291bdb6c0220ce405e437
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-11-26 10:35:11 -0300

    criando a interface do arquivo add.php

---
+++ pages/add.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <h2>
+                WebJournal
+            </h2>
+        </div>
+    </header>
+    <div class="conteiner-add-post">
+        <div class="hour">
+            <input type="text" name="date" id="date" readonly>
+        </div>
+        <div class="submit-post">
+            <input type="submit" name="submit" id="submit" value="Enviar">
+        </div>
+    </div>
+</body>
+</html>
