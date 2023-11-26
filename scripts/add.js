aa63472d438d83b3f3eca428b711a4df7bb83788
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-11-26 11:15:41 -0300

    add.js terminado

--- scripts/add.js
+++ scripts/add.js
@@ -2,5 +2,9 @@ function getDateUpload(){
     const today = new Date()
     const date = today.getDate()
     const inputDate = document.getElementById("date").value = date
+    const jsonDate = {
+        data: date
+    }
+    console.log(jsonDate)
 }
 getDateUpload()
