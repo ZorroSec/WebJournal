ff92f7ae297fdab2a3984ca412d23ae677383fcb
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-11-27 19:01:33 -0300

    criando a interface do index.ph

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
     <header class="header">
         <div class="logo">
             <h2>
-                WebJournal
+
+                <a href="index.php">WebJournal</a>
             </h2>
         </div>
     </header>
