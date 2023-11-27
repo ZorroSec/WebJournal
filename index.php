42f52825ca82860efd375531369fe9d2427975f6
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-11-27 19:02:03 -0300

    alterando a parte de title

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
+    <title>WebJournal</title>
 </head>
 <body>
     <header class="header">
         <div class="logo">
             <h2>
-
                 <a href="index.php">WebJournal</a>
             </h2>
         </div>
