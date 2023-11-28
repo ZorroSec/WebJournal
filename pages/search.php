695fbafbf96b488e926ab1f793469464f7d58001
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-11-28 08:51:51 -0300

    criando o header do search.php

--- pages/search.php
+++ pages/search.php
@@ -3,9 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v6.4.2/css/all.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>WebJournal | Search User</title>
 </head>
 <body>
-    
+    <header class="header" style="justify-content: space-between;">
+        <div class="logo">
+            <h2>
+                <a href="index.php" style="text-decoration: none; color: #23abff;">WebJournal</a>
+            </h2>
+        </div>
+        <div class="link">
+            <a href="pages/search.php" style="text-decoration: none; color: #23abff;"><i class="fa-solid fa-magnifying-glass fa-lg"></i></a>
+        </div>
+        <div class="link">
+            <a href="pages/add.php" style="text-decoration: none; color: #23abff;"><i class="fa-solid fa-plus fa-lg"></i></a>
+        </div>
+        <div class="link">
+            <a href="pages/blog.php" style="text-decoration: none; color: #23abff;"><i class="fa-solid fa-blog"></i></a>
+        </div>
+    </header>
 </body>
 </html>
